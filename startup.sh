409c442b78ecd5b15831c09f0c03a3ccef5fcefb
Ahmed Zahid <96044698+Ahmed78978@users.noreply.github.com>
2024-03-30 03:51:28 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,9 +5,17 @@
 #mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/ib_logfile1_backup
 #RUN mv /var/lib/mysql/\#innodb_redo/redo* /var/lib/mysql/
 # Start MySQL server with minimal upgrade and skip grant tables
+echo "Moving redo log files..."
+mv /var/lib/mysql/ib_logfile0 /var/lib/mysql/ib_logfile0_backup
+echo "ib_logfile0 moved"
+mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/ib_logfile1_backup
+echo "ib_logfile1 moved"
+# Start MySQL server with minimal upgrade and skip grant tables
+echo "Starting MySQL server with minimal upgrade and skip grant tables..."
 mysqld --upgrade=minimal --skip-grant-tables
-mkdir -p /var/lib/mysql/backups
-CMD mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" --password="$MYSQL_PASSWORD" \
-    --single-transaction \
-    --result-file=/var/lib/mysql/backups/backup.$(date +%F.%T).sql \
-    --all-databases
+echo "done mysql"
+#mkdir -p /var/lib/mysql/backups
+#CMD mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" --password="$MYSQL_PASSWORD" \
+   # --single-transaction \
+   # --result-file=/var/lib/mysql/backups/backup.$(date +%F.%T).sql \
+   # --all-databases
