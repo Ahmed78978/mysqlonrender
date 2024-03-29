1bc24e015edbc0c254abd03f29f8c14a7c486c40
Ahmed Zahid <96044698+Ahmed78978@users.noreply.github.com>
2024-03-30 04:28:52 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -14,7 +14,8 @@ mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/ib_logfile1_backup
 echo "ib_logfile1 moved"
 # Start MySQL server with minimal upgrade and skip grant tables
 echo "Starting MySQL server with minimal upgrade and skip grant tables..."
-mysqld --skip-grant-tables --skip-add-locks
+mysqld --skip-grant-tables --skip-host-cache --skip-name-resolve --skip-add-locks --skip-slave-start --skip-show-database --skip-log-bin --skip-log-slave-updates --skip-auto-rehash --skip-external-locking --skip-innodb
+
 
 echo "done mysql"
 #mkdir -p /var/lib/mysql/backups
