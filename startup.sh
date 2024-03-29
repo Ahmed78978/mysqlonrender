e6539924e8c867d490fd9a0d848725324879634f
Ahmed Zahid <96044698+Ahmed78978@users.noreply.github.com>
2024-03-30 03:54:20 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,9 @@
 # Move redo log files to a backup location
 #mv /var/lib/mysql/ib_logfile0 /var/lib/mysql/ib_logfile0_backup
 #mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/ib_logfile1_backup
-#RUN mv /var/lib/mysql/\#innodb_redo/redo* /var/lib/mysql/
+#mv /var/lib/mysql/\#innodb_redo/redo* /var/lib/mysql/
+mv /var/lib/mysql /var/lib/mysql_old/
+
 # Start MySQL server with minimal upgrade and skip grant tables
 echo "Moving redo log files..."
 mv /var/lib/mysql/ib_logfile0 /var/lib/mysql/ib_logfile0_backup
@@ -12,7 +14,7 @@ mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/ib_logfile1_backup
 echo "ib_logfile1 moved"
 # Start MySQL server with minimal upgrade and skip grant tables
 echo "Starting MySQL server with minimal upgrade and skip grant tables..."
-mysqld --upgrade=minimal --skip-grant-tables
+mysqld --skip-grant-tables
 echo "done mysql"
 #mkdir -p /var/lib/mysql/backups
 #CMD mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" --password="$MYSQL_PASSWORD" \
