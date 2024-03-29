61663b0bfb8b19fa879e317d2a3f371e140a763d
Ahmed Zahid <96044698+Ahmed78978@users.noreply.github.com>
2024-03-30 04:45:41 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -12,9 +12,14 @@ mv /var/lib/mysql/ib_logfile0 /var/lib/mysql/ib_logfile0_backup
 echo "ib_logfile0 moved"
 mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/ib_logfile1_backup
 echo "ib_logfile1 moved"
+mkdir -p /var/lib/mysql/backups
+echo "backup created"
 # Start MySQL server with minimal upgrade and skip grant tables
 echo "Starting MySQL server with minimal upgrade and skip grant tables..."
-mysqld --upgrade=MINIMAL --skip-grant-tables --skip-host-cache --skip-name-resolve --skip-slave-start --skip-show-database --skip-log-bin --skip-log-slave-updates --skip-external-locking --skip-innodb
+mysqld --upgrade=MINIMAL --skip-grant-tables --skip-host-cache --skip-name-resolve --skip-slave-start --skip-show-database --skip-log-bin --skip-log-slave-updates --skip-external-locking --skip-innodb && mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" --password="$MYSQL_PASSWORD" \
+    --single-transaction \
+    --result-file=/var/lib/mysql/backups/backup.$(date +%F.%T).sql \
+    --all-databases 
 
 
 echo "done mysql"
