83843168c79d1e7ba9ff050de31d5d1d3355e7ff
Ahmed Zahid <96044698+Ahmed78978@users.noreply.github.com>
2024-03-30 06:03:01 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -16,7 +16,7 @@ mkdir -p /var/lib/mysql/backups
 echo "backup created"
 # Start MySQL server with minimal upgrade and skip grant tables
 echo "Starting MySQL server with minimal upgrade and skip grant tables..."
-mysqld --upgrade=MINIMAL --skip-grant-tables --skip-host-cache --skip-name-resolve --skip-slave-start --skip-show-database --skip-log-bin --skip-log-slave-updates --skip-external-locking --skip-innodb 
+mysqld --skip-grant-tables --skip-host-cache --skip-name-resolve --skip-slave-start --skip-show-database --skip-log-bin --skip-log-slave-updates --skip-external-locking --skip-innodb 
 
 
 echo "done mysql"
