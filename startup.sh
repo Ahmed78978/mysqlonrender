1ae4c78a4c0fec86677b2a196194a7e12c59c84a
Ahmed Zahid <96044698+Ahmed78978@users.noreply.github.com>
2024-03-30 19:35:08 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -13,6 +13,8 @@ echo "ib_logfile0 moved"
 #mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/ib_logfile1_backup
 echo "ib_logfile1 moved"
 #mkdir -p /var/lib/mysql/backups
+zip -r mysql_data.zip /var/lib/mysql
+
 echo "backup created"
 # Create a new folder named mysql_old in /var/lib/
 #mkdir /var/lib/mysql_old
@@ -22,7 +24,7 @@ echo "backup created"
 
 # Start MySQL server with minimal upgrade and skip grant tables
 echo "Starting MySQL server "
-mysqld --innodb-force-recovery=4 --skip-lock-tables
+mysqld --innodb-force-recovery=4 
 
 
 echo "done mysql"
